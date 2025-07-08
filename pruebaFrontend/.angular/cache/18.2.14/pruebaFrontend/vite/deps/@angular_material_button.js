2b41acdbceda99073b922a388554b36ec76c2c5b
Mau <maurlop99@gmail.com>
2025-07-07 21:33:14 -0600

    “PRIMERO”

---
+++ pruebaFrontend/.angular/cache/18.2.14/pruebaFrontend/vite/deps/@angular_material_button.js
@@ -0,0 +1,34 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-YNCNWVXG.js";
+import "./chunk-N53DX23S.js";
+import "./chunk-57CLKA5D.js";
+import "./chunk-4IWFGYDR.js";
+import "./chunk-J5U464ZH.js";
+import "./chunk-L3TH4L7L.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
